19cb1d0b9228002722bc34598acb13dfc5916c9a
ehzShelter <emrulhasanzawad@outlook.com>
2016-09-19 16:08:01 +0600

    JS new and this keyword wiredness

---
+++ newKeyword.js
@@ -0,0 +1,35 @@
+// function Person(saying) {
+//   this.saying = saying;
+//   return {
+//     talk: function() {
+//       console.log("I am returning saying: ", this.saying);
+//     }
+//   }
+// }
+//
+function Person(saying) {
+  return {
+    talk: function() {
+      console.log("I am returning saying: ", saying);
+    }
+  }
+}
+
+// function Pseudonew(constructor) {
+//   var obj = {};
+//   Object.setPrototypeOf(obj, constructor.prototype);
+//   var argsArray = Array.prototype.slice.apply(arguments);
+//   constructor.apply(obj, argsArray.slice(1));
+//   return obj;
+// }
+
+// Person.prototype.talk = function() {
+//   console.log("I say: ", this.saying);
+// }
+
+
+
+// var Crockford = Pseudonew(Person, "Semicolan Semicolon!!! shit...")
+var Crockford = new Person("Semicolan Semicolon!!! shit...");
+
+Crockford.talk();
